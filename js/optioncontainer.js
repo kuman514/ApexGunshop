5c58ece6dc7d27ef74e686fce66b44e934204319
kumankoishi.html <7iggif@naver.com>
2021-02-12 18:07:30 +0900

    Removed redundant codes

--- js/optioncontainer.js
+++ js/optioncontainer.js
@@ -29,16 +29,7 @@ class optionContainer {
     let targetDiv = document.querySelector(`.option${level}`)
     console.log(targetDiv)
     switch (level) {
-      case 1: {
-          let target = targetDiv.querySelector('.comboSelect')
-          let inner = '<option value="0">선택하세요</option>'
-          this.contain[index].forEach(item => {
-            inner += `<option value="${item}">${gunTypeName.get(item)}</option>`
-          })
-          target.innerHTML = inner
-        }
-        break
-      case 2: {
+      case 1: case 2: {
           let target = targetDiv.querySelector('.comboSelect')
           let inner = '<option value="0">선택하세요</option>'
           this.contain[index].forEach(item => {
