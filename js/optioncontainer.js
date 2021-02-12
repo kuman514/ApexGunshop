5843aef10a3e2e1f208c9d1c501f439e499368fe
kumankoishi.html <7iggif@naver.com>
2021-02-12 18:06:25 +0900

    Added function of Level 2

--- js/optioncontainer.js
+++ js/optioncontainer.js
@@ -40,6 +40,11 @@ class optionContainer {
         break
       case 2: {
           let target = targetDiv.querySelector('.comboSelect')
+          let inner = '<option value="0">선택하세요</option>'
+          this.contain[index].forEach(item => {
+            inner += `<option value="${item}">${gunTypeName.get(item)}</option>`
+          })
+          target.innerHTML = inner
         }
         break
       case 3:
