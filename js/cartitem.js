7ddce414be828fa49aafd4c4c6204d54e39411cf
kumankoishi.py <7iggif@naver.com>
2021-02-14 13:53:35 +0900

    Adding Cart and CartItem

---
+++ js/cartitem.js
@@ -0,0 +1,10 @@
+class CartItem {
+  constructor (_guntype, _name, _options, _price) {
+    this.type = _guntype
+    this.name = _name
+    this.options = _options
+    this.price = _price
+  }
+  render () {
+  }
+}
