dd4b640daf582cadc41814fc1c6d6d4c8704609a
kumankoishi.py <7iggif@naver.com>
2021-02-14 14:31:49 +0900

    Adding neccessary members of Cart and CartItem

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,24 @@
 class Cart {
   constructor () {
+    this.contain = new Map()
+    this.addingItemId = 1
+  }
+  add (_guntype, _name, _options, _price, _amount, _max) {
+    this.contain.set(
+      this.addingItemId,
+      new CartItem(_guntype, _name, _options, _price, _amount, _max)
+    )
+    this.addingItemId++
+  }
+  get (index) {
+    return this.contain.get(index)
+  }
+  clear () {
+    this.contain.clear()
+  }
+  renderAllCartItems () {
+  }
+  renderTotalPrice () {
+
   }
 }
--- js/cartitem.js
+++ js/cartitem.js
@@ -1,9 +1,11 @@
 class CartItem {
-  constructor (_guntype, _name, _options, _price) {
+  constructor (_guntype, _name, _options, _price, _amount, _max) {
     this.type = _guntype
     this.name = _name
     this.options = _options
     this.price = _price
+    this.amount = _amount
+    this.maxAmount = _max
   }
   render () {
   }
